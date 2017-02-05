08ae4bf09043eded548c4c091fe178428a95b6c5
syahirah <syahirahmohammad89@gmail.com>
2017-02-05 23:12:20 +0800

    add menu-highlight

--- js/main.js
+++ js/main.js
@@ -24,6 +24,75 @@ function fixedHeader() {
  }
 }
 
+/**
+ * Menu Hightlight
+ */
+function menuHightlight() {
+    try {
+        $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top-60
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+
+        /**
+         * This part handles the highlighting functionality.
+         * We use the scroll functionality again, some array creation and
+         * manipulation, class adding and class removing, and conditional testing
+         */
+        var aChildren = $("#menu-main-top-navigation a"); // find the a children of the list items
+        var aArray = []; // create the empty aArray
+        for (var i = 0; i < aChildren.length; i++) {
+            var aChild = aChildren[i];
+            var ahref = $(aChild).attr('href');
+            if (2 > ahref.length) {
+                continue;
+            }
+            aArray.push(ahref);
+        } // this for loop fills the aArray with attribute href values
+
+        $(window).scroll(function() {
+            var windowPos = $(window).scrollTop()+90; // get the offset of the window from the top of page
+            var windowHeight = $(window).height(); // get the height of the window
+            var docHeight = $(document).height();
+
+            for (var i = 0; i < aArray.length; i++) {
+                var theID = aArray[i];
+                var div = $(theID).offset();
+                if (typeof div === 'undefined') {
+                    continue;
+                }
+                var divPos = $(theID).offset().top; // get the offset of the div from the top of page
+                var divHeight = $(theID).height(); // get the height of the div in question
+                if (windowPos >= divPos && windowPos < (divPos + divHeight)) {
+                    $("a[href='" + theID + "']").parent().addClass("activenav");
+                } else {
+                    $("a[href='" + theID + "']").parent().removeClass("activenav");
+                }
+            }
+
+            if (windowPos + windowHeight == docHeight) {
+                console.log(windowPos + windowHeight);
+                if (!$("nav li:last-child").hasClass("activenav")) {
+                    var navActiveCurrent = $(".activenav").attr("href");
+                    $("a[href='" + navActiveCurrent + "']").removeClass("activenav");
+                    $("nav li:last-child").addClass("activenav");
+                }
+            }
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
 
 
 
